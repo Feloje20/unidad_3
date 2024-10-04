a6a83994a284ee576535c349cf398abd2da11fbe
Feloje20 <115107211+Feloje20@users.noreply.github.com>
2024-10-04 13:42:02 +0200

    Update ej_01_2.php

--- array/ej_01_2.php
+++ array/ej_01_2.php
@@ -21,7 +21,7 @@ for ($i = 0; $i < 10; $i++) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercio 2 php</title>
+    <title>Ejercio 1.3 php</title>
     <style>
         .ver_codigo {
             margin-top: 50px;
