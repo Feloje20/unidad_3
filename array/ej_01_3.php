67284a3a2daac50bbdfcd3e652d4cf8a45518058
Feloje20 <115107211+Feloje20@users.noreply.github.com>
2024-10-04 13:42:20 +0200

    Update ej_01_3.php

--- array/ej_01_3.php
+++ array/ej_01_3.php
@@ -1,22 +1,16 @@
 <?php
 /**
-* 1.3 Nota de los alumnos para DWES.
+* 1.3 Nota de los alumnos para DWES. Es un array de ejemplo de como se podría hacer.
 * @author Jesús Ferrer López
 * @date 02/10/2024
 */
 
 // Inicializamos el array
 $alumnos_nota_dwes = array (
-    "Jesús" => 10,
-    "Juan" => 5,
-    "María" => 8,
-    "Ana" => 7,
-    "Pedro" => 6,
-    "Luis" => 4,
-    "Carlos" => 9,
-    "Sara" => 3,
-    "Lucía" => 2,
-    "Marta" => 1
+    array ("nombre" => "Jesús", "apellidos" => ["Ferrer", "López"], "Foto" => "foto.png", "nota" => 10),
+    array ("nombre" => "Maria", "apellidos" => ["Castillo", "López"], "Foto" => "foto.png", "nota" => 8),
+    array ("nombre" => "Alvaro", "apellidos" => ["Manchado", "Paredes"], "Foto" => "foto.png", "nota" => 6),
+    array ("nombre" => "Lucía", "apellidos" => ["Ronaldo", "López"], "Foto" => "foto.png", "nota" => 4)
 );
 
 ?>
@@ -26,7 +20,7 @@ $alumnos_nota_dwes = array (
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercio 3 php</title>
+    <title>Ejercio 1.3 php</title>
     <style>
         .ver_codigo {
             margin-top: 50px;
@@ -34,6 +28,24 @@ $alumnos_nota_dwes = array (
     </style>
 </head>
 <body>
+    <table border="1">
+        <th>nombre</th>
+        <th>apellido1</th>
+        <th>apellido2</th>
+        <th>foto</th>
+        <th>nota</th>
+        <?php
+            foreach ($alumnos_nota_dwes as $alumno) {
+                echo "<tr>";
+                echo "<td>" . $alumno["nombre"] . "</td>";
+                echo "<td>" . $alumno["apellidos"][0] . "</td>";
+                echo "<td>" . $alumno["apellidos"][1] . "</td>";
+                echo "<td>" . $alumno["Foto"] . "</td>";
+                echo "<td>" . $alumno["nota"] . "</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
     <div class="ver_codigo">
         <button type="button"><a href="">Ver código</a></button>
     </div>   
