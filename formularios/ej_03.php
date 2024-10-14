1c4307bd231fabacbb0900394a26249d88670b94
feloje20 <jefeloseneca@gmail.com>
2024-10-14 11:37:43 +0000

    Adición de carpeta formularios y ejercicios 3 y 4 completados

---
+++ formularios/ej_03.php
@@ -0,0 +1,88 @@
+<?php
+    /**
+     * 3. Formulario de curriculum con diferentes inputs.
+     * @autor Jesús Ferrer López	
+     * @date 14/10/2024
+     */
+    $lprocesaformulario = false;
+    $nombre = "";
+    $apellidos = "";
+    $email = "";
+    $curso = "";
+    $idioma = "";
+    $buscaTrabajo = "";
+    $lenguaje = "";
+
+    if((isset($_POST["enviar"]))){
+        $lprocesaformulario = true;
+    }
+
+    if ($lprocesaformulario){
+        // Recoger datos
+        $nombre = $_POST["nombre"];
+        $apellidos = $_POST["apellidos"];
+        $email = $_POST["email"];
+        $curso = $_POST["curso"];
+        $idioma = $_POST["idiomas"];
+        $buscaTrabajo = $_POST["buscaTrabajo"];
+        $lenguaje = $_POST["lenguaje_fav"];
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $lprocesaformulario = false;
+        } 
+    }
+?>
+<!-- VISTA -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if ($lprocesaformulario) {
+        // Mostrar datos
+        echo "Nombre: $nombre <br/>";
+        echo "Apellidos: $apellidos <br/>";
+        echo "email: $email <br/>";
+        echo "curso: $curso <br/>";
+        echo "idioma: $idioma <br/>";
+        if ($buscaTrabajo) {echo "Es demandante de empleo. <br/>";}
+        echo "lenguaje: $lenguaje <br/>";
+    } else {
+    ?>
+    <h1> Curriculum personal</h1>
+    <form action="" method="post">
+        <input type="text" placeholder="nombre" name="nombre" value="<?php echo $nombre?>" id=""><br/>
+        <input type="text" placeholder="apellidos" name="apellidos" value="<?php echo $apellidos?>" id=""><br/>
+        <input type="text" placeholder="email" name="email" value="<?php echo $email?>" id=""><br/>
+        <select name="curso">
+            <option>1ºDAW</option>
+            <option>2ºDAW</option>
+            <option>Big data</option>
+        </select><br/>
+        <label for="idiomas">Tus idiomas</label>
+        <select name="idiomas" multiple>
+            <option>Español</option>
+            <option>Alemán</option>
+            <option>Inglés</option>
+        </select><br/>
+        <label for="buscaTrabajo">¿Buscas trabajo?</label>
+        <input type="checkbox" name="buscaTrabajo" value="buscaTrabajo"><br/>
+        <fieldset>
+            <legend>Lenguaje favorito</legend>
+            <input type="radio" name="lenguaje_fav" value="js" checked>Java script
+            <input type="radio" name="lenguaje_fav" value="Python">Python
+            <input type="radio" name="lenguaje_fav" value="C">C++
+            <input type="radio" name="lenguaje_fav" value="Kobold">Kobold
+        </fieldset><br/>
+        <input type="submit" name="enviar" value="enviar formulario">
+        <input type="reset" name="resetear" value="resetear">
+    </form>
+    <?php
+    }
+    ?>
+</body>
+</html>
---
+++ formularios/ej_04.php
@@ -0,0 +1,57 @@
+<?php
+    /**
+     * 4. Formulario de selección de pais.
+     * @autor Jesús Ferrer López	
+     * @date 14/10/2024
+     */
+    $lprocesaformulario = false;
+    $paises = [
+        "Spain" => ["Madird", "./img/spain.jpg"],
+        "Alemania" => ["Berlin", "./img/alemania.png"],
+        "Portugal" => ["Lisboa", "./img/portugal.jpg"]
+    ];
+    $nombrePais = "";
+    $datosPais;
+
+    if((isset($_POST["enviar"]))){
+        $lprocesaformulario = true;
+    }
+
+    if ($lprocesaformulario){
+        // Recoger datos
+        $nombrePais = $_POST["country"];
+        $datosPais = $paises[$_POST["country"]];
+    }
+?>
+<!-- VISTA -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if ($lprocesaformulario) {
+        // Mostrar datos
+        echo "<h1>$nombrePais</h1>";
+        echo "capital:  $datosPais[0]<br>";
+        echo "<img src=$datosPais[1] width='250' height='250'>";
+    } else {
+    ?>
+    <h1> Formulario paises</h1>
+    <form action="" method="post">
+        <select name="country">
+            <option>Spain</option>
+            <option>Alemania</option>
+            <option>Portugal</option>
+        </select><br/>
+        <input type="submit" name="enviar" value="enviar formulario">
+        <input type="reset" name="resetear" value="resetear">
+    </form>
+    <?php
+    }
+    ?>
+</body>
+</html>
